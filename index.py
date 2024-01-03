528e6fde497e4e500b80c05170b5c4a599c0caf9
GabrielSTNS <you@example.com>
2024-01-03 15:57:44 -0300

    Substituindo as condições "if" para "elif"

--- index.py
+++ index.py
@@ -1,33 +1,33 @@
-numero1 = float(input("Digite um número: "))
-numero2 = float(input("Digite outro número: "))
+numero1 = input("Digite um número: ")
+numero2 = input("Digite outro número: ")
 conta = input("Informe o tipo de conta (+, -, *, /, **): ")
 
+conta_numero_1 = int(numero1)
+conta_numero_2 = int(numero2)
+
 if conta == "+":
-    linha_1 = (f"A soma de {numero1:.2f} com {numero2:.2f} é: ")
-    linha_2 = numero1 + numero2
+    linha_1 = (f"A soma de {conta_numero_1:.2f} com {conta_numero_2:.2f} é: ")
+    linha_2 = conta_numero_1 + conta_numero_2
     print(linha_1)
     print(f"{linha_2:.2f}")
 
 if conta == "-":
-    linha_1 = (f"A subtração de {numero1:.2f} com {numero2:.2f} é: ")
-    linha_2 = numero1 - numero2
+    linha_1 = (f"A subtração de {conta_numero_1:.2f} com {conta_numero_2:.2f} é: ")
+    linha_2 = conta_numero_1 - conta_numero_2
     print(linha_1)
     print(f"{linha_2:.2f}")
-
-if conta == "*":
-    linha_1 = (f"A multiplicação de {numero1:.2f} com {numero2:.2f} é: ")
-    linha_2 = numero1 * numero2
+elif conta == "*":
+    linha_1 = (f"A multiplicação de {conta_numero_1:.2f} com {conta_numero_2:.2f} é: ")
+    linha_2 = conta_numero_1 * conta_numero_2
     print(linha_1)
     print(f"{linha_2:.2f}")
-
-if conta == "/":
-    linha_1 = (f"A divisão de {numero1:.2f} com {numero2:.2f} é: ")
-    linha_2 = numero1 / numero2
+elif conta == "/":
+    linha_1 = (f"A divisão de {conta_numero_1:.2f} com {conta_numero_2:.2f} é: ")
+    linha_2 = conta_numero_1 / conta_numero_2
     print(linha_1)
     print(f"{linha_2:.2f}")
-
-if conta == "**":
-    linha_1 = (f"A exponenciação de {numero1:.2f} com {numero2:.2f} é: ")
-    linha_2 = numero1**numero2
+elif conta == "**":
+    linha_1 = (f"A exponenciação de {conta_numero_1:.2f} com {conta_numero_2:.2f} é: ")
+    linha_2 = conta_numero_1**conta_numero_2
     print(linha_1)
     print(f"{linha_2:.2f}")
